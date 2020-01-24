4affc2aae569dccb85e53a3c99f3853af83fa5de
luidy <luidy@gmail.com>
2020-01-24 16:55:55 -0300

    primeira confirmação

---
+++ src/components/admin/AdminPages.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="admin-pages">
+        <PageTitle 
+            icon="fa fa-cogs" 
+            main="Administração do Site" 
+            sub="Cadastros & Cia">
+        </PageTitle>
+    </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle'
+
+export default {
+    name: 'AdminPages',
+    components: { PageTitle }
+}
+</script>
+
+<style>
+
+</style>
